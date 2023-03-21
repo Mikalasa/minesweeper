f8c610382284b42bf75ea932d932e35a4bcb6634
FeiFeiMoon <htt3001ptq@hotmail.com>
2023-03-21 17:28:47 +1300

    center the text

--- src/component/Cell.jsx
+++ src/component/Cell.jsx
@@ -8,8 +8,12 @@ function Cell(props) {
         var src = "./Svgs/bomb.svg"
     }
 
+    if (props.number !== 9) {
+        var number = props.number
+    }
+
     return(
-        <div className="cell" index={props.index}><img src={src} /></div>
+        <div className="cell" index={props.index}><img src={src} /><span>{number}</span></div>
     );
 }
 
